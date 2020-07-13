8df5a56b8164551eee9d30167b84b363c880278f
Matt Jones <271films@gmail.com>
2020-07-13 16:50:08 -0400

    add a 1 sec timer between jumps

--- coin2.gd
+++ coin2.gd
@@ -1,11 +1,20 @@
 extends RigidBody2D
 
+var canJump = true
 
 func _input(event):
-	if Input.is_action_just_pressed("jump"):
+	if Input.is_action_just_pressed("jump") && canJump:
 		apply_impulse(Vector2(0, 0), Vector2(0, -600))
+		canJump = false
+		$Timer.start()
+	else:
+		pass
 
 
 func _physics_process(delta):
 	pass
 
+
+
+func _on_Timer_timeout():
+	canJump = true
